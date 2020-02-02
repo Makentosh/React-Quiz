1614cd92ecca5266564068c48425b4aaa4c061c8
Makentosh <ziki_007@ukr.net>
2020-02-02 18:49:51 +0200

    [new react project]

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import Car from "./Car";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>hello react</h1>
+        <Car name={'Audi'}/>
+        <Car name={'Ford'}>
+            <p>children element</p>
+        </Car>
     </div>
   );
 }
