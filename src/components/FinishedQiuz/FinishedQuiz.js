eadb050cc8a8a2c36572f2d227f78e6a359185a0
Makentosh <ziki_007@ukr.net>
2020-02-07 22:11:19 +0200

    finish first step

--- src/components/FinishedQiuz/FinishedQuiz.js
+++ src/components/FinishedQiuz/FinishedQuiz.js
@@ -1,5 +1,6 @@
 import React from "react";
 import classes from './FinishedQuiz.scss'
+import Button from "../UI/Button/Button";
 
 const FinishedQuiz = props => {
     const successCount = Object.keys(props.results).reduce((total, key) => {
@@ -29,8 +30,8 @@ const FinishedQuiz = props => {
             </ul>
             <p>правильно { successCount } из {props.quiz.length}</p>
             <div>
-                <button
-                onClick={props.onRetry}>Повторить</button>
+                <Button onClick={props.onRetry} type="primary">Повторить</Button>
+                <Button type="success">Перейти в список тестов</Button>
             </div>
         </div>
     )
